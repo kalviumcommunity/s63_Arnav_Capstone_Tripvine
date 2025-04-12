4fcbb632f862e02c7b5f5c1c9a88b983bd596b8c
ARNAVLIHANTU23 <Arnav.Lihantu@kalvium.community>
2025-04-12 15:17:48 +0530

    GET API Done

--- travel-stories-backend/routes/travelStories.js
+++ travel-stories-backend/routes/travelStories.js
@@ -7,9 +7,4 @@ router.get('/', (req, res) => {
   res.send('This is where all travel stories will be fetched.');
 });
 
-// Sample POST route for adding a new travel story
-router.post('/', (req, res) => {
-  res.send('This is where new travel stories will be added.');
-});
-
 module.exports = router;
